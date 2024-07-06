dfe47c7751eb251511df74ef7dddacf4b7867b50
Krish Mehra <km937@cornell.edu>
2024-07-06 13:04:47 -0400

    Clean up repo

--- backend/handler.js
+++
@@ -1,9 +0,0 @@
-const app = require('./index.js')
-
-exports.index = require('express-on-serverless')(app);
-
-// const server = serverless.createServer(app);
-
-// exports.handler = async (event, context) => {
-//   return serverless.proxy(server, event, context);
-// }
